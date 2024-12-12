9ff52359960f0bfdcf400ccda191b77ead32d030
yehwenz <qwas0903029050@gmail.com>
2024-12-12 20:07:44 +0800

    修正

--- scan/src/router/index.ts
+++ scan/src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -19,5 +20,6 @@ const router = createRouter({
     },
   ],
 })
+
 
 export default router
